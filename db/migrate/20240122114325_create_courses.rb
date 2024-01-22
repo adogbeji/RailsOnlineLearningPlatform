71958c200419f4159ec11a1295b9bb156b6b0f62
adogbeji <baka_1993@yahoo.co.uk>
2024-01-22 12:05:00 +0000

    Create first scaffold & add validation

---
+++ db/migrate/20240122114325_create_courses.rb
@@ -0,0 +1,12 @@
+class CreateCourses < ActiveRecord::Migration[6.1]
+  def change
+    create_table :courses do |t|
+      t.string :title
+      t.string :string
+      t.string :description
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
