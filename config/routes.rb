fd456df42670eb20b2cd2dfa73342a6c9a95d850
adogbeji <baka_1993@yahoo.co.uk>
2024-01-22 11:14:35 +0000

    Add home controller & update root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  # get 'home/index'
   # get 'static_pages/landing_page'
   # get 'static_pages/privacy_policy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  root 'static_pages#landing_page'
+  # root 'static_pages#landing_page'
+  root 'home#index'
   
   get 'privacy_policy', to: 'static_pages#privacy_policy'
 end
